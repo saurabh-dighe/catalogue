8a9a5b6203bd87dd2cf03bbfb4893024d87fb884
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-09 00:58:05 +0530

    auto pushhh

--- server.js
+++ server.js
@@ -178,7 +178,7 @@ function mongoLoop() {
 mongoLoop();
 
 // fire it up!
-const port = process.env.CATALOGUE_SERVER_PORT || '3000';
+const port = process.env.CATALOGUE_SERVER_PORT || '5000';
 app.listen(port, () => {
     logger.info('Started on port', port);
 });
