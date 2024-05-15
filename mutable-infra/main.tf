422b2aa36c4eaf787781a9f80ca8a770cb21eb75
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-15 23:38:32 +0530

    Auto push

--- mutable-infra/main.tf
+++ mutable-infra/main.tf
@@ -3,4 +3,8 @@ module "catalogue" {
   ENV                    = var.ENV
   INTERNAL               = var.INTERNAL
   COMPONENT              = var.COMPONENT
+  SPOT_INSTANCE_COUNT    = var.SPOT_INSTANCE_COUNT
+  SPOT_INSTANCE_TYPE     = var.SPOT_INSTANCE_TYPE
+  OD_INSTANCE_COUNT      = var.OD_INSTANCE_COUNT
+  OD_INSTANCE_TYPE       = var.OD_INSTANCE_TYPE
 }
--- mutable-infra/vars.tf
+++ mutable-infra/vars.tf
@@ -1,5 +1,11 @@
 variable "ENV" {}
-variable "INTERNAL" {}
 variable "COMPONENT" {}
+variable "INTERNAL" {}
 
+#Spot variable
+variable "SPOT_INSTANCE_COUNT" {}
+variable "SPOT_INSTANCE_TYPE" {}
 
+#OD Variable
+variable "OD_INSTANCE_COUNT" {}
+variable "OD_INSTANCE_TYPE" {}
